c0bebf02a0b8805a7d747ffc7f5da251fee384fe
Ashley Hicks <a.hicks@Ashleys-Air.lan>
2026-04-21 20:19:48 -0400

    fix event description

--- app.py
+++ app.py
@@ -58,11 +58,9 @@ events_data = [
     {
         "id": 1,
         "name": "Battle of the DJs",
-        "description": "Showcase your sound and compete for the crown.
-
-This is an indoor and outdoor event, so programming may adjust based on weather conditions. Tickets are transferable if plans change.
-
-Please note: this is a 30+ event. IDs will be checked at the door, and anyone under 30 will be denied entry with no refund.",
+        "description": """Showcase your sound and compete for the crown. 
+This is an indoor and outdoor event; in the event of inclement weather, tickets will be transferable to a future date or eligible for a refund. 
+This is a 30+ event — valid IDs will be checked at the door. Guests who do not meet the age requirement will be denied entry with no refund.""",
         "flyer": "/static/images/flyer-part1.jpg",
 
         "tickets": {
